22cab7582755c9a905e0b7d5d7c277441f9010df
jefffang19 <jefffang07@gmail.com>
2021-06-21 19:47:42 +0800

    Change ifft to ifft2

--- part_one.m
+++ part_one.m
@@ -3,5 +3,5 @@ k_space_data = load('K_space_data.mat');
 imshow(abs(k_space_data.K_space_slice18), [])
 
 % part 1.2
-X = ifft(k_space_data.K_space_slice18);
+X = ifft2(k_space_data.K_space_slice18);
 imshow(abs(X), [])
