bdc1f0c9967707584a81946b7f7ba8edcc6af02e
jefffang19 <jefffang07@gmail.com>
2021-06-23 14:34:40 +0800

    Change show image method

--- part_one.m
+++ part_one.m
@@ -1,7 +1,12 @@
 % part 1.1
 k_space_data = load('K_space_data.mat');
+figure
+subplot(1,2,1)
 imshow(abs(k_space_data.K_space_slice18), [])
+title('k-space')
 
 % part 1.2
 X = ifft2(k_space_data.K_space_slice18);
+subplot(1,2,2)
 imshow(abs(X), [])
+title('magnitude image')
